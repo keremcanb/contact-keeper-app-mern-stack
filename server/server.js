46a151b338e2b3db9875258d26aa4aa82f06a4e8
keremcanb <keremcan@gmail.com>
2021-02-21 01:03:29 +0300

    refactor

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('../config/db');
 const path = require('path');
 
 const app = express();
@@ -18,7 +18,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('/client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
